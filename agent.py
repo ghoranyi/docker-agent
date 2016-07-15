88ac66ab13cff570cdd50da3590de2ce252aebc9
Gergo Horanyi <gergo@pipetop.com>
2016-07-15 15:06:41 +0200

    Improving logging

--- agent.py
+++ agent.py
@@ -1,10 +1,15 @@
 import datetime
 from docker import Client
+import logging
 import os
 import requests
 import schedule
 import simplejson
 import time
+
+
+log = logging.getLogger("dockeragent")
+logging.basicConfig(level=logging.INFO)
 
 
 def get_client():
@@ -25,10 +30,11 @@ def register_node():
             url = "{backend}:{port}/containers/api/register".format(backend=get_backend_url(), port=get_backend_port())
             response = requests.get(url)
             if response.status_code == 200:
+                log.info("Node registered, id: {txt}".format(txt=response.text))
                 return response.text
             raise Exception()
         except:
-            print("Failed to register node. Retry in 10s.")
+            log.warn("Failed to register node. Retry in 10s.")
             time.sleep(10)
 
 
@@ -44,17 +50,17 @@ def send_container_list(node_id):
         }
         response = requests.post(url, data=simplejson.dumps(data))
         container_names = [c["Image"] for c in containers]
-        print("[{timestamp}] {response} {containers}".format(
+        log.info("[{timestamp}] {response} {containers}".format(
             timestamp=datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S"),
             response=response.status_code,
             containers=','.join(container_names)
         ))
     except:
-        print("[{timestamp}] FAILED to send data.".format(
+        log.warn("[{timestamp}] FAILED to send data.".format(
             timestamp=datetime.datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S")))
 
 node_id = register_node()
-send_container_list()
+
 schedule.every().minute.do(send_container_list, node_id=node_id)
 
 while True:
